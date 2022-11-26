91e0716155ce79c8972ba15b7343875e7d0e8a54
«Alexander» <semeon.gromow@yandex.ru>
2022-11-26 20:17:19 +0300

    Homework_1 task №8

---
+++ Exp004/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+
+Console.Write("Введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+int i = n;
+if (n == 0)
+{
+  Console.Write("вы ввели ноль");
+}
+ 
+ if (n > 0)
+{
+        while (i > 1)
+    {
+        Console.Write($"{n - i + 2} ");
+        i = i - 2;
+    }
+}
+
+else
+{
+    while (i < -1)
+    {
+        Console.Write($"{n + (-i - 2)} ");
+        i = i + 2;
+    }
+}
+
+
+
